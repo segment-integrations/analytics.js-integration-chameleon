4901fd8a117efe3e3fa11a1a67a3021ff2f5d23a
Brian Norton <brian.nort@gmail.com>
2017-06-07 12:52:48 -0700

    Modernize test harness; add Browserify support

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var Analytics = require('analytics.js-core').constructor;
-var integration = require('analytics.js-integration');
-var sandbox = require('clear-env');
-var tester = require('analytics.js-integration-tester');
+var Analytics = require('@segment/analytics.js-core').constructor;
+var integration = require('@segment/analytics.js-integration');
+var sandbox = require('@segment/clear-env');
+var tester = require('@segment/analytics.js-integration-tester');
 var Chameleon = require('../lib/');
 
 var noop = function() {};
@@ -62,7 +62,7 @@ describe('Chameleon', function() {
       });
 
       it('should add the current location', function() {
-        analytics.assert.equal(/http:\/\/localhost:\d+\/test/.test(window.chmln.location), true);
+        analytics.assert.equal(/http:\/\/localhost:\d+\//.test(window.chmln.location), true);
       });
 
       it('should load', function() {
@@ -107,35 +107,8 @@ describe('Chameleon', function() {
       });
 
       it('should store the identify', function() {
-        analytics.assert(window.chmln.identify_a[0].length === 1);
-
-        analytics.assert.deepEqual({ uid: 'id' }, window.chmln.identify_a[0][0]);
-      });
-    });
-
-    describe('on alias', function() {
-      describe('with the new id', function() {
-        beforeEach(function() {
-          analytics.alias('new');
-        });
-
-        it('should store the alias', function() {
-          analytics.assert(window.chmln.alias_a[0].length === 1);
-
-          analytics.assert.deepEqual({ from: 'anon-id', to: 'new' }, window.chmln.alias_a[0][0]);
-        });
-      });
-
-      describe('with the both old and new id', function() {
-        beforeEach(function() {
-          analytics.alias('new', 'old');
-        });
-
-        it('should store the alias', function() {
-          analytics.assert(window.chmln.alias_a[0].length === 1);
-
-          analytics.assert.deepEqual({ from: 'old', to: 'new' }, window.chmln.alias_a[0][0]);
-        });
+        analytics.assert.equal(Array.prototype.slice.call(window.chmln.identify_a).length, 1);
+        analytics.assert.equal('id', window.chmln.identify_a[0][0]);
       });
     });
   });
@@ -158,45 +131,24 @@ describe('Chameleon', function() {
         analytics.spy(window.chmln, 'identify');
       });
 
-      it('should identify with the anonymous user id', function() {
+      it('should identify with no id', function() {
         analytics.identify();
-        analytics.called(window.chmln.identify, { uid: 'anon-id' });
+        analytics.called(window.chmln.identify, null, {});
       });
 
       it('should identify with the given id', function() {
         analytics.identify('id');
-        analytics.called(window.chmln.identify, { uid: 'id' });
+        analytics.called(window.chmln.identify, 'id');
       });
 
       it('should send traits', function() {
         analytics.identify({ trait: true });
-        analytics.called(window.chmln.identify, { uid: 'anon-id', trait: true });
+        analytics.called(window.chmln.identify, null, { trait: true });
       });
 
       it('should send the given id and traits', function() {
         analytics.identify('id', { trait: true });
-        analytics.called(window.chmln.identify, { uid: 'id', trait: true });
-      });
-    });
-
-    describe('#group', function() {
-      beforeEach(function() {
-        analytics.stub(window.chmln, 'set');
-      });
-
-      it('should send an id', function() {
-        analytics.group('id');
-        analytics.called(window.chmln.set, { 'group:id': 'id' });
-      });
-
-      it('should send traits', function() {
-        analytics.group({ trait: true });
-        analytics.called(window.chmln.set, { 'group:id': null, 'group:trait': true });
-      });
-
-      it('should send an id and traits', function() {
-        analytics.group('id', { trait: true });
-        analytics.called(window.chmln.set, { 'group:id': 'id', 'group:trait': true });
+        analytics.called(window.chmln.identify, 'id', { trait: true });
       });
     });
 
@@ -213,23 +165,6 @@ describe('Chameleon', function() {
       it('should send an event and properties', function() {
         analytics.track('event', { property: true });
         analytics.called(window.chmln.track, 'event', { property: true });
-      });
-    });
-
-    describe('#alias', function() {
-      beforeEach(function() {
-        analytics.user().anonymousId('anon-id');
-        analytics.stub(window.chmln, 'alias');
-      });
-
-      it('should send a new id', function() {
-        analytics.alias('new');
-        analytics.called(window.chmln.alias, { from: 'anon-id', to: 'new' });
-      });
-
-      it('should send a new and old id', function() {
-        analytics.alias('new', 'old');
-        analytics.called(window.chmln.alias, { from: 'old', to: 'new' });
       });
     });
   });
